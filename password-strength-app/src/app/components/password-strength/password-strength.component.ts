5fba5696781319a82a6fd7c298ff8f39a745d88a
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2024-07-13 21:55:22 +0300

    add regex

--- password-strength-app/src/app/components/password-strength/password-strength.component.ts
+++ password-strength-app/src/app/components/password-strength/password-strength.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnChanges,  SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges,  SimpleChanges} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { regex } from '../../utils/regex';
 
 @Component({
   selector: 'app-password-strength',
@@ -8,9 +9,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './password-strength.component.html',
   styleUrls: ['./password-strength.component.css']
 })
+  
 export class PasswordStrengthComponent implements OnChanges{
   @Input() password: string = '';
   strength: string = 'empty';
+
+   hasLetters = regex.letters.test(this.password);
+   hasNumbers = regex.numbers.test(this.password);
+   hasSymbols = regex.symbols.test(this.password);
    
  ngOnChanges(changes: SimpleChanges): void {
     if (changes['password']) {
@@ -18,20 +24,27 @@ export class PasswordStrengthComponent implements OnChanges{
     }
   }
 
-  calculateStrength(password: string) {
-    if (password.length === 0) {
-      this.strength = 'empty';
-    } else if (password.length < 8) {
-      this.strength = 'short';
-    } else if (/^[a-zA-Z]+$/.test(password) || /^[0-9]+$/.test(password) || /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/.test(password)) {
-      this.strength = 'easy';
-    } else if (/[a-zA-Z]/.test(password) && /[0-9]/.test(password) && /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
-      this.strength = 'strong';
-    } else if ((/[a-zA-Z]/.test(password) && /[0-9]/.test(password)) ||
-               (/[a-zA-Z]/.test(password) && /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) ||
-               (/[0-9]/.test(password) && /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(this.password))) {
-      this.strength = 'medium';
-    }
+calculateStrength(password: string) {
+  const hasLetters = regex.letters.test(password);
+  const hasNumbers = regex.numbers.test(password);
+  const hasSymbols = regex.symbols.test(password);
+
+  if (password.length === 0) {
+    this.strength = 'empty';
+  } else if (password.length < 8) {
+    this.strength = 'short';
+  } else if (hasLetters && !hasNumbers && !hasSymbols || 
+             !hasLetters && hasNumbers && !hasSymbols || 
+             !hasLetters && !hasNumbers && hasSymbols) {
+    this.strength = 'easy';
+  } else if ((hasLetters && hasNumbers && !hasSymbols) || 
+             (hasLetters && !hasNumbers && hasSymbols) || 
+             (!hasLetters && hasNumbers && hasSymbols)) {
+    this.strength = 'medium';
+  } else if (hasLetters && hasNumbers && hasSymbols) {
+    this.strength = 'strong';
   }
+}
+
 }
 
---
+++ password-strength-app/src/app/utils/regex.ts
@@ -0,0 +1,5 @@
+export const regex = {
+  letters: /[a-zA-Z\u0400-\u04FF]/,
+  numbers: /[0-9]/,
+  symbols: /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/
+};
